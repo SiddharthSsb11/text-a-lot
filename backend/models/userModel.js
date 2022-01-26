a3fd4d44f2dcfa783af3775a0d5e561bbac9b5a6
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-26 23:58:11 +0530

    Models created

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: "String", required: true },
+    email: { type: "String", unique: true, required: true },
+    password: { type: "String", required: true },
+    pic: {
+      type: "String",
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestaps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
