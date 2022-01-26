9e029ebf5b0d6f555db2f25eea13957f98ca560b
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-27 03:58:11 +0530

    db created and connected

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -5,10 +5,7 @@ const chatModel = mongoose.Schema(
     chatName: { type: String, trim: true },
     isGroupChat: { type: Boolean, default: false },
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    latestMessage: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Message",
-    },
+    latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message"},
     groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
+dotenv.config();
+connectDB(); 
 const app = express();
 
 
+app.get('/', (req, res) => {
+    console.log('hello from server');
+    res.send('Api running');
+})
 
-app.listen(5000, console.log('Server started'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`.yellow.bold));
