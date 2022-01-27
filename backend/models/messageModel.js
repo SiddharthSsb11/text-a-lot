37db4bdaf30b3f36d61bfac13aef0ead66f04ba6
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-28 03:17:29 +0530

    seen by removed

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -5,7 +5,7 @@ const messageSchema = mongoose.Schema(
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     content: { type: String, trim: true },
     chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
-    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    /* readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], */
   },
   { timestamps: true }
 );
