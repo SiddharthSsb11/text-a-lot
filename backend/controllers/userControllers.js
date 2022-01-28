9b98d05a2e8377804c262a58b290495e86742a79
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-29 01:20:49 +0530

    user search logic added

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,17 +2,41 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
 
+//@description     Get or Search all users
+//@route           GET /api/user?search=
+//@access          Public
+
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } }, //case insensitive
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  //console.log(keyword,'search keyword'); //{} 
+  
+  //user id from protect MW
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } }); 
+  //not including the logged in user
+  //console.log(users, 'matching search users');
+  res.send(users);
+});
 
 //@description     Auth/login the user
 //@route           POST /api/user/login
 //@access          Public
+
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
   //console.log('login user', user)
 
-  if (user && (await user.matchPassword(password))) { //instance method available on docs of certain collection
+  if (user && (await user.matchPassword(password))) {
+    //instance method available on docs of certain collection
 
     res.json({
       _id: user._id,
@@ -30,6 +54,7 @@ const authUser = asyncHandler(async (req, res) => {
 //@description     Register new user
 //@route           POST /api/user/
 //@access          Public
+
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -67,4 +92,4 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, authUser };
+module.exports = { registerUser, authUser, allUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userControllers");
+const { registerUser, authUser, allUsers } = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+router.route("/").get(protect, allUsers);
 router.route("/").post(registerUser);
 router.post("/login", authUser);
 
