8655bf6eec32d379490444b4119006859e74ecec
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-29 22:52:06 +0530

    production ready code added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
@@ -7,9 +8,8 @@ const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 
-
 dotenv.config();
-connectDB(); 
+connectDB(); //mongodb connection
 const app = express();
 
 app.use(express.json()); // to accept json data from req body sent from fe 
@@ -18,15 +18,31 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 
 
+//deployment
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
  
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     //console.log('hello from server');
     res.send('Api running');
 })
-
+ */
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`.yellow.bold));
